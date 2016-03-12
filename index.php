8dae79ef5e8134f13f074040775ef99024051da5
Ry Ferguson <rytf12@gmail.com>
2016-03-12 15:42:23 -0800

    for each blog post display the title, date, author and content

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
         <h1><?php bloginfo('name')?></h1>
         <h2><?php bloginfo('description')?></h2>
     </div>
+    <?php
+
+        while(have_posts()) {
+	    the_post(); ?>
+            <div class="blog-post">
+                <h3><?php the_title()?></h3>
+		<p class="blog-info"><?php the_date()?> by <?php the_author()?></p>
+		<?php the_content()?>
+            </div>
+	<?php }
+
+    ?>
 <?php wp_footer()?>
 </body>
 </html>

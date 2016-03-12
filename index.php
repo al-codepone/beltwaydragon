4a3f3a92a256a29e4086c00093be035e077292a3
Ry Ferguson <rytf12@gmail.com>
2016-03-12 12:51:08 -0800

    remove space at end of template PHP calls

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="<?=get_stylesheet_uri()?>"/>
-    <?php wp_head() ?>
+    <?php wp_head()?>
 </head>
 <body>
     <div id="header">
-        <h1><?php bloginfo('name') ?></h1>
-        <h2><?php bloginfo('description') ?></h2>
+        <h1><?php bloginfo('name')?></h1>
+        <h2><?php bloginfo('description')?></h2>
     </div>
-<?php wp_footer() ?>
+<?php wp_footer()?>
 </body>
 </html>

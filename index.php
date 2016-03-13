afe60d4de629fb17a4a0fe7c754ad947652e4300
Ry Ferguson <rytf12@gmail.com>
2016-03-12 16:10:14 -0800

    move opening PHP tag up one line

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
                 <h3><?php the_title()?></h3>
                 <p class="blog-info"><?php the_date()?> by <?php the_author()?></p>
                 <?php the_content()?>
-            </div>
-	<?php }
+            </div> <?php
+        }
 
     ?>
 <?php wp_footer()?>

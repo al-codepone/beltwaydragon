08ba1069fbc9240575464fd0a26eccb0fc312a60
Ry Ferguson <rytf12@gmail.com>
2016-03-14 14:13:13 -0700

    use single separate template file for whole page

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <?php wp_head()?>
-</head>
-<body>
-    <div id="header">
-        <h1><?php bloginfo('name')?></h1>
-        <h2><?php bloginfo('description')?></h2>
-    </div>
-    <?php
+<?php
 
-        while(have_posts()) {
-	    the_post(); ?>
-            <div class="blog-post">
-                <h3><?php the_title()?></h3>
-                <p class="blog-info"><?php the_date()?> by <?php the_author()?></p>
-                <?php the_content()?>
-            </div> <?php
-        }
+ob_start();
 
-    ?>
-<?php wp_footer()?>
-</body>
-</html>
+while(have_posts()) {
+    the_post(); ?>
+    <div class="blog-post">
+        <h3><?php the_title()?></h3>
+        <p class="blog-info"><?php the_date()?> by <?php the_author()?></p>
+        <?php the_content()?>
+    </div> <?php
+}
+
+$t_content = ob_get_clean();
+include TEMPLATEPATH . '/template.php';
---
+++ template.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php wp_head()?>
+</head>
+<body>
+    <div id="header">
+        <h1><?php bloginfo('name')?></h1>
+        <h2><?php bloginfo('description')?></h2>
+    </div>
+    <?=$t_content?>
+<?php wp_footer()?>
+</body>
+</html>
